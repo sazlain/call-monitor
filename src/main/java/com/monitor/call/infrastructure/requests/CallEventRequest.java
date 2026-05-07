f20e988c8a2760838374bcc58a9c59528a4b40e1
sazlain <azlain.saavedra@gmail.com>
2026-05-07 17:30:23 -0500

    fix params request

--- src/main/java/com/monitor/call/infrastructure/requests/CallEventRequest.java
+++ src/main/java/com/monitor/call/infrastructure/requests/CallEventRequest.java
@@ -1,6 +1,8 @@
 package com.monitor.call.infrastructure.requests;
 
 import com.google.gson.Gson;
+import com.monitor.call.domain.enums.CallFlow;
+import com.monitor.call.domain.enums.CallStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,8 +19,8 @@ public class CallEventRequest {
     private String callerIdName;
     private String calledDID;
     private String calledExtension;
-    private String callStatus;
-    private String callFlow;
+    private CallStatus callStatus;
+    private CallFlow callFlow;
     private String callerExtension;
     private String calledNumber;
     private String callAPIID;
